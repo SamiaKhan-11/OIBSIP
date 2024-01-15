1e6ab7e4c5aac3b40569f94baefce3aae10fa2ec
SamiaKhan-11 <155620580+SamiaKhan-11@users.noreply.github.com>
2024-01-15 22:25:57 +0530

    Added main

---
+++ portfolio website/script.js
@@ -0,0 +1,6 @@
+src="https://unpkg.com/typed.js@2.1.0/dist/typed.umd.js"
+var typed = new Typed('#element', {
+    strings: ['Web Developer', '&amp; a Graphic Designer'],
+    typeSpeed: 50,
+    loop: true,
+});
